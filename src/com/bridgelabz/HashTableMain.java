a41ece12476376d0c809698f7449329e3ecf96f4
TahirMansuri <tprocks786@gmail.com>
2022-01-22 18:28:28 +0530

    MODIFIED: Remove given Word by Using Hash Table

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,9 +1,13 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class HashTableMain {
     public static void main(String[] args) {
         //HashTableExp Object for Generic Type
         HashTableExp<String> hashTableExp = new HashTableExp<>();
+
+        Scanner sc = new Scanner(System.in);
 
         String str = "to be or not to be that to not be or";
 
@@ -11,7 +15,7 @@ public class HashTableMain {
 
         //Integer strArr[] ={1,6,9,3,7,1,9,6,3,9};
 
-        //System.out.println("Given String ="+str);
+        System.out.println("Given String ="+str);
         //Passing Array Element in HashTable Index
         for(String mystr : strArr) {
             //Get index of each object
@@ -21,6 +25,14 @@ public class HashTableMain {
         }
 
         //Methode to Show Hash Table Data
+        hashTableExp.showHashList();
+
+        //Code to Remove the Given Word from HashTable Linked List
+        System.out.println("\nEnter the Word to Remove from Hash Table :");
+        String word = sc.next();
+        int index = hashTableExp.myHashFunction(word);
+        hashTableExp.removeKey(index,word);
+
         hashTableExp.showHashList();
     }
 }
